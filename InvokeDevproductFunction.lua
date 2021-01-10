f65857f120571a6d629692ce49f856fd86503786
kyungaa <77231415+kyungaa@users.noreply.github.com>
2021-01-10 06:18:23 -0600

    2 new files

---
+++ InvokeDevproductFunction.lua
@@ -0,0 +1,36 @@
+local buyService = GetService(DevProductService)
+local httpService = GetService(HttpService)
+
+local function invoke-purchase(buyService,httpService)
+{
+
+    local table = [<ID=YourProductId><AUTH=AuthToken>]
+
+    buyService.PromptPurchase(ExtractFromTable(table<ID>)).new
+    {
+
+        pcall function new()
+        (
+
+            local function InvokeErrorMessageEvent(result)
+            (
+                <error logic here>
+            )
+
+            onError.Return:InvokeErrorMessageEvent()
+
+        )
+        else
+        (
+    
+            parent.AssumeSuccess(success)
+
+        )
+
+        private result = parent.OutcomeMsg
+
+        print(result)
+
+    }
+
+}
---
+++ TeleportFunction.lua
@@ -0,0 +1,24 @@
+local teleportService = GetService(TeleportService)
+local authKey = GetService(UuidGenerator(Raw))
+local place = teleportService.FetchId(Fireteam-Boarding)
+
+local function Teleport(teleportService,authKey,place)
+{
+
+    teleportService:Invoke(1:place,2:authKey)
+    [
+
+        if (parent.Fail..(errorMessage))
+        (
+            print(errorMessage)
+        )
+        else
+        (
+            print(“success”)
+        )
+
+    ]
+
+}
+
+Player.Teleporting:InvokeEvent(teleport)
